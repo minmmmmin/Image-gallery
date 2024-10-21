6ed31b954d7f111216a4eb3a5dd819609e958a03
minmmmmin <kotomi.hinahima@gmail.com>
2024-10-21 10:33:23 +0900

    とりあえずなんかたくさん作ってみた

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,26 @@ export default function App() {
             src="images/pic1.jpg"
             alt="Closeup of a human eye"
           />
+          <img
+           className="displayed-img"
+           src="images/pic2.jpg"
+           alt="Rock that looks like a wave"
+        />
+          <img
+           className="displayed-img"
+           src="images/pic3.jpg"
+           alt="Purple and white pansies"
+        />
+          <img
+           className="displayed-img"
+           src="images/pic4.jpg"
+           alt="Section of wall from a pharoah's tomb"
+        />
+          <img
+           className="displayed-img"
+           src="images/pic5.jpg"
+           alt="Large moth on a leaf"
+        />
           <div className="overlay"></div>
           <button className="dark">Darken</button>
         </div>
