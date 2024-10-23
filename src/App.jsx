1527851bec9bdedadfb43f471ea209a0c9ceb489
minmmmmin <kotomi.hinahima@gmail.com>
2024-10-23 20:06:20 +0900

    画像が横並びになりました

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,24 @@
 export default function App() {
+  const imageArray = [
+    { src: "images/pic1.jpg", alt: "Closeup of a human eye" },
+    { src: "images/pic2.jpg", alt: "Rock that looks like a wave" },
+    { src: "images/pic3.jpg", alt: "Purple and white pansies" },
+    { src: "images/pic4.jpg", alt: "Section of wall from a pharaoh's tomb" },
+    { src: "images/pic5.jpg", alt: "Large moth on a leaf" },
+  ];
+
     return (
       <>
         <h1>Image gallery example</h1>
-        <div className="full-img">
+        <div className="thumb-bar">
+        {imageArray.map((image, index) => (
           <img
-            className="displayed-img"
-            src="images/pic1.jpg"
-            alt="Closeup of a human eye"
+            key={index}
+            className="thumbnail"
+            src={image.src}
+            alt={image.alt}
           />
-          <img
-           className="displayed-img"
-           src="images/pic2.jpg"
-           alt="Rock that looks like a wave"
-        />
-          <img
-           className="displayed-img"
-           src="images/pic3.jpg"
-           alt="Purple and white pansies"
-        />
-          <img
-           className="displayed-img"
-           src="images/pic4.jpg"
-           alt="Section of wall from a pharoah's tomb"
-        />
-          <img
-           className="displayed-img"
-           src="images/pic5.jpg"
-           alt="Large moth on a leaf"
-        />
+        ))}
           <div className="overlay"></div>
           <button className="dark">Darken</button>
         </div>
